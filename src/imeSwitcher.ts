f8b05deb3a88810954ef1fb835ec77d6139f48dc
asumanino <asumanino@gmail.com>
2025-12-03 10:55:37 +0900

    devcontainerでも半角全角切替が機能するように修正

--- src/imeSwitcher.ts
+++ src/imeSwitcher.ts
@@ -14,9 +14,29 @@ export class ImeSwitcher {
   private exePath: string;
   constructor(private readonly config: ImeSwitcherConfig, context: vscode.ExtensionContext) {
     const arch = process.arch;
+    const WARN_KEY = "ime.warn";
 
     if (this.config.enable && os.platform() === "win32") {
-      this.exePath = context.asAbsolutePath(path.join("bin", `win32-${arch}`, "zenhan.exe"));
+      switch (arch) {
+        case "arm64":
+          if (!context.globalState.get(WARN_KEY)) {
+            vscode.window.showWarningMessage(`[IME] ARM64 Windows detected. Using x64 binary fallback.`);
+            context.globalState.update(WARN_KEY, true);
+          }
+          this.exePath = context.asAbsolutePath(path.join("bin", `win32-x64`, "zenhan.exe"));
+          break;
+        case "x64":
+        case "ia32":
+          this.exePath = context.asAbsolutePath(path.join("bin", `win32-${arch}`, "zenhan.exe"));
+          break;
+        default:
+          if (!context.globalState.get(WARN_KEY)) {
+            vscode.window.showWarningMessage(`[IME] Unsupported CPU architecture "${arch}"`);
+            context.globalState.update(WARN_KEY, true);
+          }
+          this.exePath = "";
+          break;
+      }
     } else {
       this.exePath = "";
     }
